c5bfabe19cf5edeae75712b47b4249fd4b7ff741
clxyify <98536033+clxyify@users.noreply.github.com>
2022-04-26 09:49:47 -0400

    Update autoexec.cs

--- autoexec.cs
+++ autoexec.cs
@@ -6,3 +6,20 @@ public void AutoExecute()
    API.Execute(readscript);
    API.Execute("print'this version sucks plz use a better one soon my friend.......\nbtw i might upgrade this later in the future uwu who knows?'");
 }
+
+try//catch error
+{
+   AutoExecute();//run attempt
+}
+catch (Exception x)//f**k u error >:(
+{
+   API.Execute("warn'error while autoexecuting\nmaybe the directory doesnt exist?\nor the script contains nothing?");
+   API.Execute("print('" + x.Message + "')");//display error message
+   MessageBox.Show("Attempt to fix error?","error");
+   Directory.CreateDirectory("autoexec");
+   File.Create("autoexec\\autoexec.lua");
+   File.WriteAllText(scriptroot, "print'autoexec testing'");
+   MessageBox.Show("Error should be fixed!", "fixed?");
+   //tryattempt
+   AutoExecute();//hopefully succeed this time
+}
